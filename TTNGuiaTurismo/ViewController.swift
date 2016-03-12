9236b4d0b161e01e833665eb3144cd13f6244636
Usuário Convidado <Guest@L2304MICRO29.local>
2016-03-12 09:22:28 -0300

    dicionando mapa e localizacao FIAP

--- TTNGuiaTurismo/ViewController.swift
+++ TTNGuiaTurismo/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    let locationManager: CLLocationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        // Define minha localizacao e centraliza o mapa nela
+        let fiapLocation:CLLocationCoordinate2D  = CLLocationCoordinate2DMake(-23.573978, -46.623272)
+
+        self.mapView.region = MKCoordinateRegionMakeWithDistance(fiapLocation, 1200, 1200)
     }
 
     override func didReceiveMemoryWarning() {
